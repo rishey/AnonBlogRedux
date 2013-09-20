362687cadb38acc455203757ee51f912d040696d
richard bluestein <richard@bluestein.com>
2013-09-19 19:34:33 -0500

    last commit for scaling back to go for simple MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -15,3 +15,22 @@ get '/single_post/:post_id' do
 	erb :single_post
 end
 
+get '/edit_add/:post_id' do
+	@post = Post.find(params[:post_id])
+	erb :edit_add
+end
+
+
+############### POSTS ###########
+
+post '/edit' do
+	p " XXXXXXXXXXXXXXXXXXXXXXXXXX PARAMS FOLLOW NEXT XXXXXXXXXXXXXXXXXXXXXXXXXX"
+	p params[:post]
+	p params[:post][:title]
+		p "XXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+ # Post.where(params[:post]).exists?
+	p Post.find_or_create_by(params[:post])
+
+	redirect '/'
+end
